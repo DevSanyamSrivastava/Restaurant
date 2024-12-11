6d53a147e79c07d462abc7e0bb7587b32c0254f1
DevSanyamSrivastava <sanyam.fullstackdev@gmail.com>
2024-12-11 16:18:54 +0530

    Add project files.

---
+++ Restaurant/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+//using Restaurant.Models;
+
+namespace Restaurant.Controllers
+{
+    public class HomeController : Controller
+    {
+        //mywebEntities con = new mywebEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+         public ActionResult Menu()
+        {
+            return View();
+        }
+             public ActionResult Catering()
+        {
+            return View();
+        }
+
+        public ActionResult Galley()
+        {
+            return View();
+        }
+        public ActionResult Feedback()
+        {
+            return View();
+        }
+
+    
+    }
+
+
+
+
+}
